2b2b77c286f4c0a1e250659139234b59fbd59419
RASOLOZANANYNOEL <hnrasolozanany@gmail.com>
2024-03-17 14:42:10 +0100

    utilisation de proptypes si on n'utilise pas typescript

--- src/Components/UI/PageTitle/PageTitle.js
+++ src/Components/UI/PageTitle/PageTitle.js
@@ -1,3 +1,5 @@
+import PropTypes from "prop-types";
+
 const PageTitle = ({ count, title }) => {
     return (
         <h2>{count} {title}</h2>
@@ -5,3 +7,14 @@ const PageTitle = ({ count, title }) => {
 };
 
 export default PageTitle;
+
+//décrire les types des props si on utilise pas typescript
+PageTitle.propTypes = {
+    count: PropTypes.number.isRequired,
+    title: PropTypes.string,
+};
+
+//sinon on précise une valeur par défault de title sans mettre required
+PageTitle.defaultProps = {
+    title: '',
+};
