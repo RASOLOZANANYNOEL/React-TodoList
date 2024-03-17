9091944592560de10d4e243fdc014b89e05c3efd
RASOLOZANANYNOEL <hnrasolozanany@gmail.com>
2024-03-17 16:13:09 +0100

    SET UP REACT ROUTER OLD AND NEW VERSION + useParams and useNavigate

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
+import { Outlet } from "react-router-dom";
+//import { Outlet, Route, Routes } from "react-router-dom"; ancienne methode
 import Header from "./Components/Header/Header";// header au lieu de style
-import Tasks from "./Components/Tasks/Tasks";
-import TasksContextProvider from "./Contexts/TasksContext";
+//import Tasks from "./Components/Tasks/Tasks";
+//import Tasks from "./Components/Tasks/Tasks";
+//import TasksContextProvider from "./Contexts/TasksContext";
 
+//nouvelle methode avec les routes version récente!
 const App = () => {
 
 
   return (
     <>
       <Header />
-      <TasksContextProvider>
-        <section className='container' style={{ position: 'relative' }}>
-          <Tasks />
-        </section>
-      </TasksContextProvider>
+      <Outlet />
     </>
   );
 };
-
 export default App;
+
+//ancienne méthode beaucoup utiliser en ce moment
+/*const App = () => {
+
+  return (
+    <>
+      <Header />
+      <Routes>
+        <Route path= "/" element= { <Tasks/>}>
+        <Route path= "/about" element= { <About/>}>
+        <Route path= "/about/:name" element= { <About/>}>
+      </Routes>
+    </>
+  );
+};*/
+
+
 
 /*<PageTitle count={0} title='Task' />
       <Button  >New Task</Button>*/
--- src/index.js
+++ src/index.js
@@ -3,13 +3,55 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import About from './Components/About/About';
+import TasksContextProvider from './Contexts/TasksContext';
+import Tasks from './Components/Tasks/Tasks';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+//mise en place de router, methode récente
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        path: '',
+        element: <Tasks />
+      },
+      {
+        path: 'about',
+        element: <About />
+      },
+      {
+        path: 'about/:name',
+        element: <About />
+      },
+
+    ]
+  },
+]);
+
+//nouvelle méthode récente
 root.render(
   <React.StrictMode>
-    <App />
+    <TasksContextProvider>
+      <RouterProvider router={router} />
+    </TasksContextProvider>
   </React.StrictMode>
 );
+
+//ancienne  méthode 
+/*root.render(
+  <React.StrictMode>
+    <TasksContextProvider>
+      <BrowserRouter>
+        <App/>
+      </BrowserRouter>
+    </TasksContextProvider>
+  </React.StrictMode>
+);*/
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
